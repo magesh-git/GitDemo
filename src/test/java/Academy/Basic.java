23344c9464ba01d28f15440f3b4fafc007196f43
Magesh Kumar <magesh3187@gmail.com>
2022-02-20 19:39:20 +0530

    gitstuff

---
+++ src/test/java/Academy/Basic.java
@@ -0,0 +1,21 @@
+package Academy;
+
+import org.testng.annotations.Test;
+
+public class Basic {
+	
+	  @Test
+		public void WiperTest()
+
+		{
+			System.out.println("Car Wiper is working fine");
+		}
+
+		@Test
+		public void DoorLockTest()
+
+		{
+			System.out.println("Car DoorLock is working fine");
+		}
+	}
+
